95b6ef6007ca4139ede55ff6c27ce9fe3ab8e4a3
Jason Peebles <jsp@Salo.local>
2015-03-24 19:37:20 -0700

    Initial Commit

---
+++ GifOClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GifOClock
+//
+//  Created by Jason Peebles on 2015-03-24.
+//  Copyright (c) 2015 Themis Solutions Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
